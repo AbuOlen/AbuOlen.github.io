114a497113f682c4f4cd616500f3f04aa76f6666
AbuOlen <do.mriyi@gmail.com>
2020-09-15 13:15:44 +0300

    level 1 sea battle imlemented. 2 warships.

---
+++ PROCode/Sea_battle/js/main.js
@@ -0,0 +1,77 @@
+class Warship {
+  constructor(Team_name, Health_points, Damage) {
+    this.Health_points = Health_points;
+    this.Damage = Damage;
+    this.is_sunk = false;
+    this.Team_name = Team_name;
+  }
+  shoot(warship) {
+    if (Math.floor(Math.random() * 100) < 20) {
+      warship.Health_points -= this.Damage;
+      console.log(this.Team_name + " hit " + warship.Team_name + " with damage " + this.Damage);
+      if(warship.Health_points <= 0) {
+        warship.is_sunk = true;
+        console.log(warship.Team_name + " ship sunk");
+      }
+    } else {
+        console.log(this.Team_name + " miss");
+    }
+  }
+}
+
+function find_ship(team) {
+  for (let i = 0; i < 1; i++) {
+    if (!team[i].is_sunk) {
+      return team[i];
+    }
+  }
+  return null;
+}
+
+const warship1 = new Warship("Team1", 100, 4);
+const warship2 = new Warship("Team2", 40, 15);
+
+let team1 = [warship1];
+let team2 = [warship2];
+
+while (true) {
+    let ws1 = find_ship(team1);
+    if (ws1 == null) {
+        console.log('*************Team1 lost**************');
+        break;
+    };
+    let ws2 = find_ship(team2);
+    if (ws2 == null) {
+        console.log('*************Team2 lost**************');
+        break;
+    }
+    ws1.shoot(ws2);
+    ws2 = find_ship(team2);
+    if (ws2 == null) {
+        console.log('**************Team2 lost**************');
+        break;
+    };
+    ws2.shoot(ws1);
+  
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
