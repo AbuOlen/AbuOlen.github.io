73fd00d9a4b4c1f2f280802453fce94a0450cd84
AbuOlen <do.mriyi@gmail.com>
2020-10-08 22:28:50 +0300

    timer countDown with animation & disabled btn Play

--- PROCode/timer_js/js/main.js
+++ PROCode/timer_js/js/main.js
@@ -1,41 +1,78 @@
-
-
-const btnSet = document.querySelector('.button_play');
+const btnSet = document.querySelector(".button_play");
+btnSet.disabled = true;
+let h = document.querySelector(".hours");
+let min = document.querySelector(".minutes");
+let sec = document.querySelector(".seconds");
 let countDownTime = new Date();
 let distance = 0;
 let now = new Date();
-// const cal = document.getElementById("appt");
-// cal.min = now.getHours;
-// cal.max = now.getHours + 1;
+let timerId = null;
+let isPaused = false;
 
-setTimer = () => {
-    distance -= 1000;
 
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+const setTimer = () => {
+  distance -= 1000;
+  if (distance < 0) {
+    distance = 0;
+    stopTimer();
+    h.classList.add("blink");
+    min.classList.add("blink");
+    sec.classList.add("blink");
+    document.querySelector(".timer-animation").classList.remove("go-anim");
+    document.querySelector(".timer-animation").classList.add("pause-anim");
+  }
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.querySelector(".minutes").innerHTML = minutes;
-    document.querySelector(".seconds").innerHTML = seconds;
-    
+  h.innerHTML = hours.toString().padStart(2, "0");
+  min.innerHTML = minutes.toString().padStart(2, "0");
+  sec.innerHTML = seconds.toString().padStart(2, "0");
 };
 
+const stopTimer = () => {
+  clearInterval(timerId);
+};
 
-
-btnSet.addEventListener('click', () => {
+const visualCountDown = () => {
+  if (timerId === null) {
     let endDate = new Date();
     let now = new Date();
     let hm = document.getElementById("appt").value;
-    let a = hm.split(':');
+    let a = hm.split(":");
     endDate.setHours(+a[0]);
     endDate.setMinutes(+a[1]);
 
-    distance = endDate.getTime() - now.getTime();
+    if (endDate.getTime() < now.getTime()) {
+      distance = endDate.getTime() - now.getTime() + 1000 * 60 * 60 * 24;
+    } else {
+      distance = endDate.getTime() - now.getTime();
+    }
 
-    setInterval(setTimer, 1000);
-});
+    timerId = setInterval(setTimer, 1000);
+    btnSet.value = "\u01C1";
+    document.querySelector(".timer-animation").classList.add("go-anim");
 
+  } else {
+        if(isPaused) {
+            timerId = setInterval(setTimer, 1000);
+            btnSet.value = "\u01C1";
 
+            document.querySelector(".timer-animation").classList.remove("pause-anim");
+            document.querySelector(".timer-animation").classList.add("go-anim");
+        } else {
+            clearInterval(timerId);
+            btnSet.value = "\ue807";
 
+            document.querySelector(".timer-animation").classList.remove("go-anim");
+            document.querySelector(".timer-animation").classList.add("pause-anim");
+        }
+        isPaused = !isPaused;
+  }
+};
 
-
+document.getElementById("appt").addEventListener("change", () => {
+    btnSet.disabled = (typeof document.getElementById("appt").value === 'undefined');
+})
+btnSet.addEventListener("click", visualCountDown);
 
