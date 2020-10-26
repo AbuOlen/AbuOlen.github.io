0f268b1ed19a44d42e4d4dff634c03756d56f7c4
AbuOlen <do.mriyi@gmail.com>
2020-10-26 22:45:47 +0200

    fixed addres images

--- PROCode/node_request_download-image/js/main.js
+++ PROCode/node_request_download-image/js/main.js
@@ -6,8 +6,8 @@ const url = 'https://dou.ua/';
 
 const parseUrl = (r, html, startIdx) => {
   const sliced = html.substring(startIdx);
-  let idx1 = sliced.indexOf('https://s.dou.ua/img/announces/');
-  const tmp = sliced.slice(idx1, idx1 + 80);
+  let idx1 = sliced.indexOf('https://s.dou.ua/CACHE/images/img/announces/');
+  const tmp = sliced.slice(idx1, idx1 + 200);
   let idx2 = tmp.indexOf('"');
   r.push(tmp.substring(0, idx2));
   return idx1 + idx2;
@@ -17,9 +17,10 @@ request(url, (err, res, body) => {
   page = body;
   //console.log(body);
   const result = [];
-  let sIdx = 0;
+  let sIdx = body.indexOf('<h3>Советуем почитать</h3>');
   for (let i = 0; i < 6; i++) {
     sIdx += parseUrl(result, body, sIdx);
   }
   console.table(result);
-};
+});
+
